b51f0f9f1302e6ead49a0c5fab1468ad01275139
salathiel1999 <55575006+salathiel1999@users.noreply.github.com>
2024-05-28 23:27:01 -0600

    Update db1.php

--- db1.php
+++ db1.php
@@ -1,4 +1,4 @@
 <?php
  
-    $conexion = mysqli_connect('localhost', 'root', '', 'transportes'); //conexion con la base de datos
+    $conexion = mysqli_connect('xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'j522kzw3x00hubbu', 'l9cr1co12buozt2i', 'v2oyuk7czp2eateq'); //conexion con la base de datos
     ?>
