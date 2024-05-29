e338a12f6f157f977073034cfc73a4ee6a6674dd
salathiel1999 <55575006+salathiel1999@users.noreply.github.com>
2024-05-28 23:28:16 -0600

    Update base_de_datos.php

--- base_de_datos.php
+++ base_de_datos.php
@@ -6,7 +6,7 @@ $contraseña = "l9cr1co12buozt2i";
 $usuario = "j522kzw3x00hubbu";
 $nombre_base_de_datos = "v2oyuk7czp2eateq"; //nombre de tu base de datos PDO::PARAM_STR
 try{
-	$base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+	$base_de_datos = new PDO('mysql:host=xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 	 $base_de_datos->query("set names utf8;");
     $base_de_datos->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
