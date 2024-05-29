18c6d5ab7e20f50246ac38678595a5d4438f2bbf
salathiel1999 <55575006+salathiel1999@users.noreply.github.com>
2024-05-28 23:25:19 -0600

    Update db2.php

--- db2.php
+++ db2.php
@@ -2,11 +2,11 @@
 /*
 Carrito de compras, archivo de conexion a la bd
 */
-$contraseña = "";
-$usuario = "root";
-$nombre_base_de_datos = "transportes"; //nombre de tu base de datos
+$contraseña = "l9cr1co12buozt2i";
+$usuario = "j522kzw3x00hubbu";
+$nombre_base_de_datos = "v2oyuk7czp2eateq"; //nombre de tu base de datos
 try{
-	$base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
+	$base_de_datos = new PDO('mysql:host=xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 	 $base_de_datos->query("set names utf8;");
     $base_de_datos->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
