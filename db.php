20251ef75da844b9b0b882c85b4eab6002eb4f7b
salathiel1999 <55575006+salathiel1999@users.noreply.github.com>
2024-05-28 23:23:38 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
  global $enlace; //variable de tipo global para llamarla en cualquier parte de la aplicacion donde se llame
  function conexion(){
-    $enlace = mysqli_connect('localhost', 'root', '', 'transportes'); //conexion con la base de datos
+    $enlace = mysqli_connect('xefi550t7t6tjn36.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'j522kzw3x00hubbu', 'l9cr1co12buozt2i', 'v2oyuk7czp2eateq'); //conexion con la base de datos
     
     if(!$enlace){ //if $enlace esto da siempre true (Si tenemos error en la conexion por eso pone !)
      echo "Error: No se puede conectara MySQL." . PHP_EOL;
